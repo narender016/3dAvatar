b79fb723c701ee67abb3c4528ae9a334b556417c
Narender Singh <65656990+narender016@users.noreply.github.com>
2024-02-16 19:51:56 +0530

    Add files via upload

---
+++ build/env.js
@@ -0,0 +1,6 @@
+window.env = {
+  "REACT_APP_ALTER_API": "ay2yf57jbtyicu6r44i5xpy2tv62hgp33xli3hsajyidhbo3nmiblgi",
+  "GENERATE_SOURCEMAP": "false",
+  "AZURE_KEY": "236592d4f27e4596ab06175f6462d04d",
+  "AZURE_REGION": "eastus"
+};
